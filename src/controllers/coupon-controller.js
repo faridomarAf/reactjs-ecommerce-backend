72b2304ee6570d7fb2cec5d5cc94d01bf8d94013
Farid Omar <fariedomar43@gmail.com>
2025-03-20 01:51:35 +0530

    create API to check for valid coupon

--- src/controllers/coupon-controller.js
+++ src/controllers/coupon-controller.js
@@ -25,7 +25,42 @@ const getCoupon = async (req, res, next) => {
 };
 
 
+const validateCoupon = async (req, res, next) => {
+    try {
+        const { code } = req.body;
+
+        if (!code) {
+            return next(new AppError("Coupon code is required!", StatusCodes.BAD_REQUEST));
+        }
+
+        const coupon = await Coupon.findOne({ code, userId: req.user._id, isActive: true });
+
+        if (!coupon) {
+            return next(new AppError("Coupon not found!", StatusCodes.NOT_FOUND));
+        }
+
+        if (coupon.expirationDate < new Date()) {
+            coupon.isActive = false;
+            await coupon.save();
+            return next(new AppError("Coupon expired!", StatusCodes.GONE));
+        }
+
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            data: {
+                code: coupon.code,
+                discountPercentage: coupon.discountPercentage,
+            },
+            message: "Coupon validated successfully!",
+        });
+    } catch (error) {
+        next(new AppError(error.message || "Failed to validate coupon", StatusCodes.INTERNAL_SERVER_ERROR));
+    }
+};
+
+
+
 module.exports = {
     getCoupon,
-
+    validateCoupon
 }
--- src/routes/v1/couponRoutes.js
+++ src/routes/v1/couponRoutes.js
@@ -6,5 +6,6 @@ const {CouponController} = require('../../controllers');
 const router = express.Router();
 
 router.get('/',AuthMiddleware.protectRoute, CouponController.getCoupon);
+router.get('/validate',AuthMiddleware.protectRoute, CouponController.validateCoupon);
 
 module.exports = router;
