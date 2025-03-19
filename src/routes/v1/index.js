209cf0e3a898fcaa62ce0f015003022aa84ae179
Farid Omar <fariedomar43@gmail.com>
2025-03-20 01:03:44 +0530

    create model for coupon

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authRoutes = require('./authRoutes');
 const productRoutes = require('./productRoutes');
 const cartRoutes = require('./cartRoutes');
+const couponRoutes = require('./couponRoutes');
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ const router = express.Router();
 router.use('/auth', authRoutes)
 router.use('/products', productRoutes)
 router.use('/cart', cartRoutes)
+router.use('/coupons', couponRoutes)
 
 module.exports = router;
