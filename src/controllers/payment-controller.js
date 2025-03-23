d2083bfaa8bc7b0d2218b512fac474e3ff43a198
Farid Omar <fariedomar43@gmail.com>
2025-03-24 00:35:46 +0530

    create API for Success checkout

--- src/controllers/payment-controller.js
+++ src/controllers/payment-controller.js
@@ -49,8 +49,15 @@ const createCheckoutSession = async(req, res, next)=>{
             discounts: coupon ? [{coupon: await createStripeCoupon(coupon.discountPercentage)},] : [],
             metadata:{
                 userId: req.user._id.toString(),
-                couponCode: couponCode || ""
-            } , 
+                couponCode: couponCode || "",
+                products: JSON.stringify(
+                    products.map((p)=>({
+                        id: p._id,
+                        quantity: p.quantity,
+                        price: p.price
+                    }))
+                )
+            }, 
         });
 
         //create coupon and add it to DB
@@ -90,6 +97,56 @@ async function createNewCoupon(userId){
     return newCoupon;
 };
 
+
+//create checkout-success
+const checkoutSuccess = async(req, res)=>{
+    try {
+        const {sessionId} = req.body;
+        const session = await Stripe.checkout.sessions.retrive(sessionId);
+
+        if(session.payment_status === "paid"){
+
+            if (session.metadata.couponCode){
+				await Coupon.findOneAndUpdate(
+					{
+						code: session.metadata.couponCode,
+						userId: session.metadata.userId,
+					},
+					{
+						isActive: false,
+					}
+				);
+            };
+
+			// create a new Order
+			const products = JSON.parse(session.metadata.products);
+			const newOrder = new Order({
+				user: session.metadata.userId,
+				products: products.map((product) => ({
+					product: product.id,
+					quantity: product.quantity,
+					price: product.price,
+				})),
+				totalAmount: session.amount_total / 100, // convert from cents to dollars,
+				stripeSessionId: sessionId,
+			});
+
+			await newOrder.save();
+
+            res.status(StatusCodes.CREATED).json({
+				success: true,
+				message: "Payment successful, order created, and coupon deactivated if used.",
+				orderId: newOrder._id,
+            });
+        };
+
+    } catch (error) {
+        next(new AppError('Error processing successfull checkout!', StatusCodes.INTERNAL_SERVER_ERROR));
+    }
+};
+
+
 module.exports ={
     createCheckoutSession,
+    checkoutSuccess,
 };
--- src/routes/v1/paymentRoutes.js
+++ src/routes/v1/paymentRoutes.js
@@ -6,5 +6,6 @@ const { AuthMiddleware} = require('../../middlewares');
 const router = express.Router();
 
 router.post('/create-checkout-session', AuthMiddleware.protectRoute,PaymentController.createCheckoutSession);
+router.post('/checkout-success', AuthMiddleware.protectRoute,PaymentController.checkoutSuccess);
 
 module.exports = router;
